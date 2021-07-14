f74a4721743a504b3b2883d9b817dd0d464f2a9d
olivervinh@gmail.com <olivervinh@gmail.com>
2021-07-14 23:30:38 +0700

    ok

---
+++ android-frontend/app/src/main/java/com/example/myapplication34/SearchAllArticlesActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication34;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchAllArticlesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_all_articles);
+    }
+}
